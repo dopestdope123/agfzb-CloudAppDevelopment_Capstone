e537b48a704dcbe1b8b0881015f669dfd85be68c
dopestdope123 <ptqmtl117@gmail.com>
2022-12-02 14:26:23 -0500

    update

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -19,27 +19,22 @@ def get_request(url, **kwargs):
         status_code = response.status_code
         print("With status {} ".format(status_code))
         json_data = json.loads(response.text)
+        return json_data
     except Exception as e:
         print("Error ", e)
+
+
+# Create a `post_request` to make HTTP POST requests
+# e.g., response = requests.post(url, params=kwargs, json=payload)
+def post_request(url, payload, **kwargs):
+    print(kwargs)
+    print("POST to {} ".format(url))
+    print(payload)
+    response = requests.post(url, params=kwargs, json=payload)
     status_code = response.status_code
     print("With status {} ".format(status_code))
     json_data = json.loads(response.text)
     return json_data
-
-# Create a `post_request` to make HTTP POST requests
-# e.g., response = requests.post(url, params=kwargs, json=payload)
-def post_request(url, json_payload, **kwargs):
-    print(json_payload)
-    print("POST from {} ".format(url))
-    try:
-        response = requests.post(url, params=kwargs, json=json_payload)
-        status_code = response.status_code
-        print("With status {} ".format(status_code))
-        json_data = json.loads(response.text)
-        print(json_data)
-        return json_data
-    except:
-        print("Network exception occurred")
 
 # Create a get_dealers_from_cf method to get dealers from a cloud function
 # def get_dealers_from_cf(url, **kwargs):
